58f3748c626fad154cbf99abfe4832a02d003806
Arnav-Ghatti <arnavghatti123@gmail.com>
2021-03-24 12:50:31 +0530

    Layout changed

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def save_json(data):
         json.dump(transactions_history, file, indent=4)
 
 def check_fields():
-    if sender_input.get() == "" or reciever_input.get() == "" or desc_input.get() == "":
+    if sender_input.get() == "" or reciever_input.get() == "" or desc_input.get("1.0", tk.END) == "":
         return False
     return True
 
@@ -36,7 +36,7 @@ def clear_fields():
     sender_input.delete(0, tk.END)
     reciever_input.delete(0, tk.END)
     amount_input.delete(0, tk.END)
-    desc_input.delete(0, tk.END)
+    desc_input.delete("1.0", tk.END)
 
 def add_transactions():
     """Adds transactios to the listbox"""
@@ -48,7 +48,7 @@ def add_transactions():
         return
 
     if check_fields():
-        transactions.append([sender_input.get(), reciever_input.get(), amount_input.get(), desc_input.get()])
+        transactions.append([sender_input.get(), reciever_input.get(), amount_input.get(), desc_input.get("1.0", tk.END)])
         transactions_history["Transactions"] = transactions
 
         clear_fields()
@@ -73,6 +73,7 @@ def delete_transaction():
 
 def load_transactions():
     """Loads data of transactions from the selected item in the listbox"""
+
     try:
         selected_idx = listbox.curselection()[0]
         selected_item = transactions[selected_idx]
@@ -82,14 +83,15 @@ def load_transactions():
         sender_var.set(selected_item[0])
         reciever_var.set(selected_item[1])
         amount_var.set(selected_item[2])
-        desc_var.set(selected_item[3])
+        desc_input.delete("1.0", tk.END)
+        desc_input.insert(tk.END, selected_item[3])
 
 def update_transactions():
     """Updates selected transaction to the details newly entered"""
     
     if check_fields():
         try:
-            transactions[listbox.curselection()[0]] = [sender_var.get(), reciever_var.get(), amount_var.get(), desc_var.get()]   
+            transactions[listbox.curselection()[0]] = [sender_var.get(), reciever_var.get(), amount_var.get(), desc_input.get("1.0", tk.END)]   
         except IndexError:
             messagebox.showwarning(title="❌ Error ❌", message="Please select any item")
         else:
@@ -102,76 +104,75 @@ def update_transactions():
 
 
 # Title
-title = tk.Label(root, text="Money Tracker", font=(FONT_NAME, 13, "normal"))
-title.pack()
+title = tk.Label(root, text="Money Tracker", font=(FONT_NAME, 15, "normal"))
+title.grid(row=0, column=0, columnspan=2, pady=3)
 
 # ---------------------------- ENTRIES AND LABELS ------------------------------- #
 input_frame = tk.Frame(root)
-input_frame.pack()
+input_frame.grid(row=1, column=0, pady=3)
 
 # Sender
-sender_label = tk.Label(input_frame, text="Sender: ", font=(FONT_NAME, 10, "normal"))
+sender_label = tk.Label(input_frame, text="Sender: ", font=(FONT_NAME, 12, "normal"))
 sender_label.grid(row=0, column=0, sticky="W")
 sender_var = tk.StringVar()
-sender_input = tk.Entry(input_frame, textvariable=sender_var, width=36, font=(FONT_NAME, 10, "normal"))
+sender_input = tk.Entry(input_frame, textvariable=sender_var, width=36, font=(FONT_NAME, 12, "normal"))
 sender_input.focus()
 sender_input.grid(row=0, column=1, sticky="W", pady=2)
 
 # Reciever
-reciever_label = tk.Label(input_frame, text="Reciever: ",  font=(FONT_NAME, 10, "normal"))
+reciever_label = tk.Label(input_frame, text="Reciever: ",  font=(FONT_NAME, 12, "normal"))
 reciever_label.grid(row=1, column=0, sticky="W")
 reciever_var = tk.StringVar()
-reciever_input = tk.Entry(input_frame, textvariable=reciever_var, width=36, font=(FONT_NAME, 10, "normal"))
+reciever_input = tk.Entry(input_frame, textvariable=reciever_var, width=36, font=(FONT_NAME, 12, "normal"))
 reciever_input.grid(row=1, column=1, sticky="W", pady=2)
 
 # Amount
-amount_label = tk.Label(input_frame, text="Amount: ", font=(FONT_NAME, 10, "normal"))
+amount_label = tk.Label(input_frame, text="Amount: ", font=(FONT_NAME, 12, "normal"))
 amount_label.grid(row=2, column=0, sticky="W")
 amount_var = tk.StringVar()
-amount_input = tk.Entry(input_frame, width=36, font=(FONT_NAME, 10, "normal"))
+amount_input = tk.Entry(input_frame, textvariable=amount_var, width=36, font=(FONT_NAME, 12, "normal"))
 amount_input.grid(row=2, column=1, sticky="W", pady=2)
 
 # Description
-desc_label = tk.Label(input_frame, text="Description: ", font=(FONT_NAME, 10, "normal"))
-desc_label.grid(row=3, column=0, sticky="W")
-desc_var = tk.StringVar()
-desc_input = tk.Entry(input_frame, textvariable=desc_var, width=36, font=(FONT_NAME, 10, "normal"))
+desc_label = tk.Label(input_frame, text="Description: ", font=(FONT_NAME, 12, "normal"))
+desc_label.grid(row=3, column=0, sticky="N")
+desc_input = tk.Text(input_frame, width=36, height=8, font=(FONT_NAME, 12, "normal"))
 desc_input.grid(row=3, column=1, sticky="W", pady=2)
 
 # ---------------------------- BUTTONS ------------------------------- #
 btn_frame = tk.Frame(root)
-btn_frame.pack()
+btn_frame.grid(row=2, column=0)
 
 # Add
-add_btn= tk.Button(btn_frame, text="    Add    ", command=add_transactions, font=(FONT_NAME, 10, "normal"))
+add_btn= tk.Button(btn_frame, text="    Add    ", command=add_transactions, font=(FONT_NAME, 11, "normal"))
 add_btn.pack(side=tk.LEFT, padx=3, pady=2)
 
 # Update
-update_btn = tk.Button(btn_frame, text="  Update  ", command=update_transactions, font=(FONT_NAME, 10, "normal"))
+update_btn = tk.Button(btn_frame, text="  Update  ", command=update_transactions, font=(FONT_NAME, 11, "normal"))
 update_btn.pack(side=tk.LEFT, padx=3, pady=2)
 
 # Delete
-del_btn = tk.Button(btn_frame, text="  Delete  ", command=delete_transaction, font=(FONT_NAME, 10, "normal"))
+del_btn = tk.Button(btn_frame, text="  Delete  ", command=delete_transaction, font=(FONT_NAME, 11, "normal"))
 del_btn.pack(side=tk.LEFT, padx=3, pady=2)
 
 # Load
-load_btn = tk.Button(btn_frame, text="   Load   ", command=load_transactions, font=(FONT_NAME, 10, "normal"))
+load_btn = tk.Button(btn_frame, text="   Load   ", command=load_transactions, font=(FONT_NAME, 11, "normal"))
 load_btn.pack(side=tk.LEFT, padx=3, pady=2)
 
 # Refresh
-refresh_btn = tk.Button(btn_frame, text="  Refresh  ", command=set_listbox, font=(FONT_NAME, 10, "normal"))
+refresh_btn = tk.Button(btn_frame, text="  Refresh  ", command=set_listbox, font=(FONT_NAME, 11, "normal"))
 refresh_btn.pack(side=tk.LEFT, padx=3, pady=2)
 
 # ---------------------------- LISTBOX ------------------------------- #
 data_frame = tk.Frame(root)
-data_frame.pack()
+data_frame.grid(row=1, column=1, rowspan=2)
 
 # Scroll Bars
 scroll_bar_y = tk.Scrollbar(data_frame, orient=tk.VERTICAL)
 scroll_bar_x = tk.Scrollbar(data_frame, orient=tk.HORIZONTAL)
 
 # Listbox
-listbox = tk.Listbox(data_frame, height=12, width=47, yscrollcommand=scroll_bar_y.set, xscrollcommand=scroll_bar_x.set, font=(FONT_NAME, 10, "normal"))
+listbox = tk.Listbox(data_frame, height=13, width=45, yscrollcommand=scroll_bar_y.set, xscrollcommand=scroll_bar_x.set, font=(FONT_NAME, 12, "normal"))
 
 # Scroll Bars
 scroll_bar_y.config(command=listbox.yview)
@@ -183,7 +184,7 @@ listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=1, pady=2)
 
 # ---------------------------- STATUS BAR ------------------------------- #
 status_frame = tk.LabelFrame(root, bd=1, relief=tk.SUNKEN)
-status_frame.pack(fill=tk.BOTH, expand=1, padx=2, pady=2)
+status_frame.grid(padx=2, pady=2, sticky=tk.N+tk.S+tk.E+tk.W, columnspan=2)
 
 # Made By
 made_by = tk.Label(status_frame, text="Made By Arnav Ghatti", anchor=tk.E, font=(FONT_NAME, 9, "normal"))
